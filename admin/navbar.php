fb582c6345d4166759524d450fa6c5d97411bf1f
kalpeshpokarde <kalpeshpokarde1011@gmail.com>
2025-10-02 22:22:28 +0530

    Create navbar.php

---
+++ admin/navbar.php
@@ -0,0 +1,13 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container">
+    <a class="navbar-brand" href="dashboard.php">Admin Panel</a>
+    <div class="collapse navbar-collapse">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item"><a class="nav-link" href="manage_user.php">Manage Users</a></li>
+        <li class="nav-item"><a class="nav-link" href="manage_slots.php">Manage Slots</a></li>
+        <li class="nav-item"><a class="nav-link" href="report.php">Reports</a></li>
+        <li class="nav-item"><a class="nav-link" href="../php/auth.php?logout=1">Logout</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
